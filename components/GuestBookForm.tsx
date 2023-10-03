6a76a41c796c8ab3f2709027a87dae0f4aea2785
bao ann <baoanh1buinguyen@gmail.com>
2023-10-03 14:26:21 +0800

    guestbook submit

--- components/GuestBookForm.tsx
+++ components/GuestBookForm.tsx
@@ -23,25 +23,35 @@ export default function GuestBookForm() {
                 console.log(message);
                 //data && addGuest(data);
             }}
-            onSubmit={() => setTrace(`Posting...`)}
+            onSubmit={() => {
+                setTrace(`Posting...`);
+                setName('');
+            }}
             id="guestbookform"
             className="tw-w-full tw-flex tw-flex-col tw-gap-6 tw-justify-end tw-items-center"
         >
             <h2>{trace}</h2>
             {trace != 'Thank you for being here <3' && (
                 <ImageFrame elem={LineLoose} className="tw-w-full tw-h-full">
-                    <label className="tw-w-full tw-flex tw-flex-col sm:tw-flex-row tw-items-center tw-border-white tw-border-[1px]">
+                    <label className="tw-w-full tw-flex tw-flex-col sm:tw-flex-row tw-items-center">
                         <input
                             className="tw-w-full !tw-text-black tw-p-3"
                             type="text"
                             name="name"
                             onChange={(e) => setName(e.target.value)}
                         />
-                        <div className="tw-font-bold tw-py-3 tw-px-1 tw-text-center hover:after:tw-content-['_<-'] hover:before:tw-content-['->_'] sm:tw-w-[8rem] tw-w-full tw-cursor-pointer">
+                        <div
+                            className={`tw-font-bold tw-py-3 tw-px-1 tw-text-center sm:tw-w-[8rem] tw-w-full tw-cursor-pointer tw-border-white tw-border-[1px] ${
+                                name == ''
+                                    ? 'tw-opacity-60'
+                                    : `hover:after:tw-content-['_<-'] hover:before:tw-content-['->_']`
+                            }`}
+                        >
                             <input
                                 type="submit"
                                 value="Submit"
                                 className="tw-cursor-pointer"
+                                disabled={name == '' ? true : false}
                             />
                         </div>
                     </label>
