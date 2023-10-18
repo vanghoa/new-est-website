85378c7b8e46441a5e53c35d6b88eb85f1089635
bao ann <baoanh1buinguyen@gmail.com>
2023-10-18 21:31:20 +0800

    revalidatepath

---
+++ app/api/revalidatepath/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
+import { PATH_BLOG } from '@/constants/paths';
+
+export async function GET(request: NextRequest) {
+    try {
+        const pathtorevalidate = request.nextUrl.searchParams.get('path');
+        if (!pathtorevalidate)
+            return NextResponse.json({
+                revalidated: false,
+                error: 'missing path',
+            });
+
+        revalidatePath(`${PATH_BLOG}/${pathtorevalidate}`);
+        revalidatePath(PATH_BLOG);
+
+        return NextResponse.json({
+            revalidated: true,
+            now: Date.now(),
+            path: pathtorevalidate,
+        });
+    } catch (e) {
+        console.log('co loi in revalidatepath (error): ', e);
+        return NextResponse.json({ message: [] }, { status: 400 });
+    }
+}
