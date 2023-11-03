8a31104bc855a0c1df8fba07ab970a2e8f5eb177
bao ann <baoanh1buinguyen@gmail.com>
2023-11-04 03:59:00 +0800

    increase nav button size for better mobile ux

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import { usePathname } from 'next/navigation';
 import { ReactNode, useEffect, useState } from 'react';
 import { Tooltips } from './SmallComponents';
 import { Char, Rand } from './WordProcessor';
+import Link from 'next/link';
 
 export default function Navbar() {
     const pathname = `/${usePathname().split('/')[1]}`;
@@ -35,7 +36,7 @@ export default function Navbar() {
             {
                 name: 'Info',
                 href: '/info',
-                tooltips: 'Some info about wu I am, and what I am made out of.',
+                tooltips: 'Some info about me, like ... what I am made out of.',
             },
         ],
     ];
@@ -54,13 +55,13 @@ export default function Navbar() {
         <nav className="tw-flex tw-flex-col tw-items-center tw-h-fit tw-fixed tw-w-full  tw-z-40 [&_*]:tw-font-display tw-overflow-visible tw-pointer-events-none">
             <div className="reveal_child tw-z-[-1] tw-max-w-3xl tw-flex tw-justify-around tw-w-fit tw-gap-2 sm:tw-gap-8 md:tw-gap-14 tw-bg-black tw-h-16 tw-pointer-events-auto tw-pt-2">
                 {navlist[0].map(({ name, href, tooltips }, i) => (
-                    <span
+                    <Link
                         key={`navlist${i}`}
                         className="tw-h-full tw-flex tw-items-center tw-justify-center tw-group tw-min-w-[8.5rem]"
+                        href={href}
                     >
                         <Char
-                            isLink
-                            rest={{ href: href }}
+                            elem={'span'}
                             className={` ${
                                 current == href
                                     ? `after:tw-content-['_<--'] before:tw-content-['-->_']`
@@ -70,33 +71,31 @@ export default function Navbar() {
                             {name}
                             <Tooltips>{tooltips}</Tooltips>
                         </Char>
-                    </span>
+                    </Link>
                 ))}
             </div>
-            <div className="reveal_child [&_a]:tw-bottom-[0.4rem] tw-z-[-2] tw-max-w-3xl tw-flex tw-justify-around tw-w-fit tw-gap-2 sm:tw-gap-8 md:tw-gap-14 tw-bg-black tw-h-12 tw-pointer-events-auto">
-                <span className="tw-h-full tw-flex tw-items-center tw-justify-center tw-group tw-min-w-[8.5rem]">
-                    <Char
-                        elem={'a'}
-                        rest={{
-                            href: '/resume/baoanhbui-resume-221023.pdf',
-                            target: '_blank',
-                        }}
-                    >
+            <div className="reveal_child [&_a>span]:tw-bottom-[0.4rem] tw-z-[-2] tw-max-w-3xl tw-flex tw-justify-around tw-w-fit tw-gap-2 sm:tw-gap-8 md:tw-gap-14 tw-bg-black tw-h-12 tw-pointer-events-auto">
+                <a
+                    className="tw-h-full tw-flex tw-items-center tw-justify-center tw-group tw-min-w-[8.5rem]"
+                    href="/resume/baoanhbui-resume-221023.pdf"
+                    target="_blank"
+                >
+                    <Char elem={'span'}>
                         Resume
                         <Tooltips>
                             a pdf file ~60kb breaking down my professional and
                             academic experiences.
                         </Tooltips>
                     </Char>
-                </span>
+                </a>
                 {navlist[1].map(({ name, href, tooltips }, i) => (
-                    <span
+                    <Link
                         key={`navlist${i}`}
                         className="tw-h-full tw-flex tw-items-center tw-justify-center tw-group tw-min-w-[8.5rem]"
+                        href={href}
                     >
                         <Char
-                            isLink
-                            rest={{ href: href }}
+                            elem={'span'}
                             className={` ${
                                 current == href
                                     ? `after:tw-content-['_<--'] before:tw-content-['-->_']`
@@ -106,16 +105,18 @@ export default function Navbar() {
                             {name}
                             <Tooltips>{tooltips}</Tooltips>
                         </Char>
-                    </span>
+                    </Link>
                 ))}
             </div>
-            <div className="[&_a]:tw-bottom-1 tw-z-[-3] tw-max-w-3xl tw-flex tw-justify-around tw-w-fit tw-gap-2 sm:tw-gap-8 md:tw-gap-14 tw-bg-black tw-h-10 tw-pointer-events-auto tw-border-t-[15px] tw-border-black tw-border-b-8 revealbg">
-                <span className="tw-w-36 tw-h-full tw-flex tw-items-center tw-justify-center tw-group">
+            <div className="[&_a>span]:tw-bottom-1 tw-z-[-3] tw-max-w-3xl tw-flex tw-justify-around tw-w-fit tw-gap-2 sm:tw-gap-8 md:tw-gap-14 tw-bg-black tw-h-10 tw-pointer-events-auto tw-border-t-[15px] tw-border-black tw-border-b-8 revealbg">
+                <a
+                    onClick={togglereveal}
+                    className="tw-w-36 tw-h-full tw-flex tw-items-center tw-justify-center tw-group"
+                >
                     <Rand
-                        elem={'a'}
+                        elem={'span'}
                         min={4}
                         className="display_none tw-font-bold tw-cursor-pointer"
-                        rest={{ onClick: togglereveal }}
                     >
                         {"``'-...__...-'``"}
                         <Tooltips className="tw-max-w-none tw-font-black">
@@ -123,17 +124,16 @@ export default function Navbar() {
                         </Tooltips>
                     </Rand>
                     <Rand
-                        elem={'a'}
+                        elem={'span'}
                         min={4}
                         className="display_on tw-hidden tw-font-bold tw-cursor-pointer"
-                        rest={{ onClick: togglereveal }}
                     >
                         {"_..-''''```''''-.._"}
                         <Tooltips className="tw-max-w-none tw-font-black">
                             {"``'-...__...-'``"}
                         </Tooltips>
                     </Rand>
-                </span>
+                </a>
             </div>
         </nav>
     );
