4eec509303db1ab66a91faab8cb4c45fee911de9
bao ann <baoanh1buinguyen@gmail.com>
2023-11-03 22:26:53 +0800

    animate presence exit animation

--- components/AnimatePageComp.tsx
+++ components/AnimatePageComp.tsx
@@ -1,7 +1,42 @@
 'use client';
-
+import React, {
+    useContext,
+    useRef,
+    ReactNode,
+    useEffect,
+    PropsWithChildren,
+} from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { ReactNode, useEffect } from 'react';
+import { LayoutRouterContext } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { usePathname } from 'next/navigation';
+
+function FrozenRouter(props: PropsWithChildren<{}>) {
+    const context = useContext(LayoutRouterContext);
+    const frozen = useRef(context).current;
+
+    return (
+        <LayoutRouterContext.Provider value={frozen}>
+            {props.children}
+        </LayoutRouterContext.Provider>
+    );
+}
+
+export function AnimatePresenceClient({ children }: { children: ReactNode }) {
+    const pathname = usePathname();
+    return (
+        <AnimatePresence mode="wait" key={'animatepres'}>
+            <motion.div
+                key={pathname}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+            >
+                <FrozenRouter>{children}</FrozenRouter>
+            </motion.div>
+        </AnimatePresence>
+    );
+}
 
 export default function AnimatePageComp({
     children,
@@ -25,16 +60,5 @@ export default function AnimatePageComp({
             upwght ? '500' : '100'
         );
     }, []);
-    return (
-        <AnimatePresence key={'animatepres'}>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 1 }}
-            >
-                {children}
-            </motion.div>
-        </AnimatePresence>
-    );
+    return <>{children}</>;
 }
